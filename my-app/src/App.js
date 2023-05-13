51909ee630cff8c9b665ffcf2db396169a2a3dea
richard <theirsaviorhd@gmail.com>
2023-05-13 15:28:49 -0400

    Admin Login User Interface
    /login page requests username and password information from admin

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  const [token, setToken] = useState('');
+
+  const handleLogin = (username, password) => {
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    })
+      .then(response => response.json())
+      .then(data => {
+        setToken(data.token);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {token ? (
+        <div>
+          <h2>You are logged in!</h2>
+          <p>Your token is: {token}</p>
+        </div>
+      ) : (
+        <LoginForm handleLogin={handleLogin} />
+      )}
     </div>
   );
 }
---
+++ my-app/src/components/LoginForm.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const LoginForm = ({ handleLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleLogin(username, password);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={({ target }) => setPassword(target.value)} />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
