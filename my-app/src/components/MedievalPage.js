512f9585f99b7e601393e820dddc656de7367162
richard <theirsaviorhd@gmail.com>
2023-05-30 10:03:35 -0400

    Medieval Accordion On Smaller Devices
    the best user experience is to have a choice between seeing copy or artworks
    if an accordion wasn't used, the user would have to scroll past the fold to see all content
    we can't guarantee the user to scroll. the accorion is a solution

--- my-app/src/components/MedievalPage.js
+++ my-app/src/components/MedievalPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Card from './Card';
 import '../styles/era-pages.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Accordion } from 'react-bootstrap';
 import { fetchArtworkData } from './artworkAPI';
 
 const MedievalPage = () => {
@@ -26,11 +26,41 @@ const MedievalPage = () => {
     filterMedievalArtworks();
   }, [artworkData]);
 
+function BasicExample() {
+  return (
+    <Accordion defaultActiveKey="0" className='d-lg-none'>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+        <div id='' className='row justify-content-center align-items-center pt-5 vh-100'>
+        <h2 className='row justify-content-center'>Medieval Art</h2>
+        <p className='era-copy col-lg-5 col-md-10'>
+          The Medieval era, spanning from the 5th to the 15th century, marked a significant transition in art history. Building upon the foundations of ancient art forms, such as Roman and Byzantine influences, Medieval art developed its distinctive style and symbolism. The era saw a fusion of Christian traditions and classical motifs, resulting in magnificent illuminated manuscripts, intricate stone carvings, and awe-inspiring stained glass windows. These artistic expressions laid the groundwork for the emergence of the Gothic art movement, characterized by soaring cathedrals, pointed arches, and elaborate sculptures. The Medieval era's reverence for spirituality and innovative architectural techniques paved the way for the grandeur and magnificence of Gothic art.
+        </p>
+      </div>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
+
   return (
     <div>
       <div className='medievalpage container col-lg-10 col-md-12 flex-column'>
         <h1 className='header pt-5'>MEDIEVAL</h1>
-        <Row className='justify-content-center'>
+        <Row className='justify-content-center d-none d-lg-flex'>
         <Row className='col-10 justify-content-center'>
             {medievalArtworks.slice(0, 10).map((artwork) => (
               <Col lg={2} className='era-crop-container'>
@@ -45,8 +75,9 @@ const MedievalPage = () => {
             ))}
         </Row>
         </Row>
+        <BasicExample className='d-flex col d-lg-none'></BasicExample>
         <a href='#era-copy' className='learn-more'>
-        <Row className='learn-more align-items-center pt-5 pb-5'>
+        <Row className='learn-more align-items-center pt-5 pb-5 d-none d-lg-flex'>
           <Col className='d-flex flex-row-reverse'>
             <img src="img/nav-down.svg" alt="nav down" />
           </Col>
@@ -59,7 +90,7 @@ const MedievalPage = () => {
         </Row>
         </a>
       </div>
-      <div id='era-copy' className='row justify-content-center align-items-center pt-5 vh-100'>
+      <div id='era-copy' className='d-none d-lg-flex row justify-content-center align-items-center pt-5 vh-100'>
         <h2 className='row justify-content-center'>Medieval Art</h2>
         <p className='era-copy col-lg-5 col-md-10'>
           The Medieval era, spanning from the 5th to the 15th century, marked a significant transition in art history. Building upon the foundations of ancient art forms, such as Roman and Byzantine influences, Medieval art developed its distinctive style and symbolism. The era saw a fusion of Christian traditions and classical motifs, resulting in magnificent illuminated manuscripts, intricate stone carvings, and awe-inspiring stained glass windows. These artistic expressions laid the groundwork for the emergence of the Gothic art movement, characterized by soaring cathedrals, pointed arches, and elaborate sculptures. The Medieval era's reverence for spirituality and innovative architectural techniques paved the way for the grandeur and magnificence of Gothic art.
