fe03f209ea2d5a843992a6cab7e2ea86ca377691
richard <theirsaviorhd@gmail.com>
2023-06-03 11:04:49 -0400

    Artwork Pages Smaller Device Style
    page is functioning on all devices as intended

--- my-app/src/components/ArtworkPage.js
+++ my-app/src/components/ArtworkPage.js
@@ -32,18 +32,26 @@ const ArtworkPage = () => {
 
   // Render the artwork page content based on the specific artwork
   return (
-    <Container className='artworkpage min-vh-100 col-lg-6 col-sm-12 flex-column'>
-      <h1 className='header pb-1 pt-5'>{thisArtwork.title}</h1>
-      <Row className='justify-content-center'>
-        <div className='card era-card col-2'>
+    <Container className='artworkpage min-vh-100 col-lg-6 col-md-12 flex-column'>
+      <h1 className='header pb-1 pt-5 d-none d-lg-flex'>{thisArtwork.title}</h1>
+      <h2 className='header-mobile h2-responsive rounded d-lg-none d-flex justify-content-center align-items-center p-4'>{thisArtwork.title}</h2>
+      <Row className='justify-content-center d-none d-lg-flex'>
+        <div className='card era-card col-lg-2'>
           <img className='img-fluid' src={'/' + thisArtwork.image} alt={thisArtwork.title} />
         </div>
         <p className='era-copy col'>{thisArtwork.description}</p>
         {/* Render other artwork details */}
       </Row>
+      <Row className='mobile-content d-lg-none pt-5 justify-content-center'>
+        <div className='card era-card col-10'>
+          <img className='img-fluid' src={'/' + thisArtwork.image} alt={thisArtwork.title} />
+        </div>
+        <p className='era-copy pt-5'>{thisArtwork.description}</p>
+        {/* Render other artwork details */}
+      </Row>
       <div className='navigation d-flex justify-content-center pb-2 pt-5 space-between'>
-        <Button className='col-3' href={`/${thisArtwork.era}`}>Back to {thisArtwork.era} Artworks</Button>
-        <Button className='col-3' href='/'>Back to Art Movements</Button>
+        <Button className='col-lg-3 col-sm-5' href={`/${thisArtwork.era}`}>Back to {thisArtwork.era} Artworks</Button>
+        <Button className='col-lg-3 col-sm-5' href='/'>Back to Art Movements</Button>
       </div>
     </Container>
   );
