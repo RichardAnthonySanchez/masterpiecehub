837da6e9ef14e7295ad13935dd43e73d9212522a
richard <theirsaviorhd@gmail.com>
2023-06-03 10:41:09 -0400

    Artwork Page Large Device Style
    artwork page is functioning as intended on large devices

--- my-app/src/components/ArtworkPage.js
+++ my-app/src/components/ArtworkPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchArtworkData } from './artworkAPI';
+import { Container, Row, Col, Accordion, Button } from 'react-bootstrap';
 
 const ArtworkPage = () => {
   const { id } = useParams();
@@ -31,12 +32,20 @@ const ArtworkPage = () => {
 
   // Render the artwork page content based on the specific artwork
   return (
-    <div>
-      <h1>{thisArtwork.title}</h1>
-      <img src={thisArtwork.image} alt={thisArtwork.title} />
-      <p>{thisArtwork.description}</p>
-      {/* Render other artwork details */}
-    </div>
+    <Container className='artworkpage min-vh-100 col-lg-6 col-sm-12 flex-column'>
+      <h1 className='header pb-1 pt-5'>{thisArtwork.title}</h1>
+      <Row className='justify-content-center'>
+        <div className='card era-card col-2'>
+          <img className='img-fluid' src={'/' + thisArtwork.image} alt={thisArtwork.title} />
+        </div>
+        <p className='era-copy col'>{thisArtwork.description}</p>
+        {/* Render other artwork details */}
+      </Row>
+      <div className='navigation d-flex justify-content-center pb-2 pt-5 space-between'>
+        <Button className='col-3' href={`/${thisArtwork.era}`}>Back to {thisArtwork.era} Artworks</Button>
+        <Button className='col-3' href='/'>Back to Art Movements</Button>
+      </div>
+    </Container>
   );
 };
 
